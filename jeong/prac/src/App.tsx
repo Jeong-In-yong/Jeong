a704686da87cc767fc0504b460a75c875af27197
Inyong Jeong <in-yong@freemind.co.jp>
2020-04-24 14:39:56 +0900

    prac

---
+++ jeong/prac/src/App.tsx
@@ -0,0 +1,47 @@
+// import React from 'react';
+// import './App.css';
+// import Child from './components/Child'
+// type Item = {
+//   id: number
+//   title: string
+// }
+
+// const App: React.FC = () => {
+//   const items: Item[] = [
+//     {
+//       id:1,
+//       title:'item1'
+//     },{
+//       id:2,
+//       title:'item2'
+//     },{
+//       id:3,
+//       title:'item3'
+//     },
+//   ]
+//   return (
+//     <div className="App">
+//       <ul>
+//         {items.map((item,i) => (
+//           <li key={item.id}>{item.id} - {item.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+  // const Child: React.FC = () => {
+  //   return (
+  //     <p>child Component</p>
+  //   )
+  // }
+
+  // const App: React.FC = () => {
+  //   return (
+  //     <div className="App">
+  //       <Child message="child Component!">
+  //         <h1>Children!</h1>
+  //       </Child>
+  //     </div>
+  //   )
+  // }
+// export default App;
---
+++ jeong/prac/src/Count.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+const Count: React.FC =() =>{
+    
+    const [count, setCount ] = useState<number>(0)
+    const handleIncrement = () => {
+        setCount(count +1)
+    }
+    return (
+        <div className="App">
+            {count}
+            <button onclick={handleIncrement}>+1</button>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <Count />,document.getElementById('root')
+);
+export default Count;
